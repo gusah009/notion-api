8b9e972ee3f7a90994c6aaa286197771ed008d67
정핸모 <gusah009@naver.com>
2024-02-05 18:26:45 +0900

    init project

---
+++ notion/src/test/kotlin/org/example/notion/NotionApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.notion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
